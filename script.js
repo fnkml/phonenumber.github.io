16da4a5471ab05f55f204dc2d4ff55abf143d5b8
fnkml <fnkml15@gmail.com>
2024-11-20 17:12:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -48,3 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 });
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
